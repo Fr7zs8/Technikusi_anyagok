b4bee8a14b54370152897bb624e1266b0a78f5f3
sipos.fruzsina <siposfruzsina07@gmail.com>
2025-09-30 10:04:29 +0200

    Asztali lancoltlista

---
+++ Asztali_alkalmazasok/2025.09.30/Lancolt_lista/Lancolt_lista/Node.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lancolt_lista
+{
+    class Node
+    {
+        public class Csucs
+        {
+            public int adat; // double? string? kiskutya?
+            public Csucs kov;
+
+        }
+
+        Csucs elso = null;
+
+        //Törlés eleje vagy adottat töröl, hozzáadás elejére, beszurás, kiiratás,   
+
+        //
+        public void Hozzaadas(int adat)
+        {   
+            Csucs cs = new Csucs();
+            cs.adat = adat;
+            cs.kov = elso;
+            elso = cs;
+        }
+
+        public static void Hozzaadas(int adat, Csucs elozo)
+        {
+            Csucs cs = new Csucs();
+            cs.adat = adat;
+            cs.kov = elozo.kov;
+            elozo.kov = cs;
+
+        }
+
+        public void Kiiras()
+        {
+            Csucs cs = elso;
+            while (cs != null)
+            {
+                Console.WriteLine(cs.adat);
+                cs = cs.kov;
+            }
+        }
+
+        public void KiirasForditva()
+        {
+            KiirasForditva(elso);
+        }
+
+        private static void KiirasForditva(Csucs cs)
+        {
+            if(cs == null)
+            {
+                return;
+            }
+            KiirasForditva(cs.kov);
+            Console.WriteLine(cs.adat);
+        }
+
+        public void Törles()
+        {
+            if(elso == null)
+            {
+                throw new InvalidOperationException("Nincs adat.");
+            }
+
+            elso = elso.kov;
+            
+        }
+
+        public void Törles(Csucs elozo)
+        {
+            if(elozo == null || elozo.kov == null)
+            {
+                throw new InvalidOperationException("Haver nincs mit törölni!!!");
+            }
+            elozo.kov = elozo.kov.kov;
+
+
+
+
+        }
+
+
+    }
+}
---
+++ Asztali_alkalmazasok/2025.09.30/Lancolt_lista/Lancolt_lista/Program.cs
@@ -0,0 +1,31 @@
+﻿ using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lancolt_lista
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Node linkedlist = new Node();
+
+            linkedlist.Hozzaadas(5);
+            linkedlist.Hozzaadas(15);
+            linkedlist.Hozzaadas(25);
+
+            linkedlist.Kiiras();
+            Console.WriteLine("-.-.-.");
+            linkedlist.KiirasForditva();
+
+            linkedlist.Törles();
+
+            Console.WriteLine("-.-.-.-.");
+            linkedlist.Kiiras();
+        }
+    }
+
+
+}
